79ff25d75126885a296adef84e48301cf6fb7d19
Sami Khan <sami999khan999@gmail.com>
2023-11-01 21:04:45 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import "./App.css";
 import FetchData from "./components/FetchData";
-import User from "./components/User";
+// import User from "./components/User";
 // import UseEffect from "./components/UseEffect";
 
 function App() {
